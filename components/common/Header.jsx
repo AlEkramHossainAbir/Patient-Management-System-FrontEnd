97d0949c63af18dee2be805c627858aa9b6767c2
Al Ekram Hossain Abir <alabir65@gmail.com>
2020-08-16 11:43:24 +0600

    check

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -58,11 +58,6 @@ export default class Header extends React.Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
-              {/* <li className="nav-item active">
-                <a className="nav-link" href="http://localhost:3000/">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li> */}
               <li className="nav-item">
                 <a className="nav-link" href="../Layout">
                   Features
